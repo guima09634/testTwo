f2c66ecdeb39d41aa2fe30ef06970d7310d132a9
danielHua <zhengjh@ap-ec.cn>
2019-08-05 11:25:25 +0800

    777777888

--- src/main/java/demo5/Demo6.java
+++ src/main/java/demo5/Demo6.java
@@ -9,6 +9,6 @@ package demo5;
 public class Demo6
 {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        System.out.println("hello world132132131!");
     }
 }
