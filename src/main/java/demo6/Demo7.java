76041db1b87fda7db6112ae91bf996cf9799a2f1
danielHua <zhengjh@ap-ec.cn>
2019-08-05 11:21:48 +0800

    55555

---
+++ src/main/java/demo6/Demo7.java
@@ -0,0 +1,13 @@
+package demo6;
+
+/**
+ * 测试demo
+ *
+ * @author daniel
+ */
+public class Demo7
+{
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
