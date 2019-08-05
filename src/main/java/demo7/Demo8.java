94aa55769d1c24cfd27a405d456c84f09861eabc
danielHua <zhengjh@ap-ec.cn>
2019-08-05 11:24:54 +0800

    777777

---
+++ src/main/java/demo7/Demo8.java
@@ -0,0 +1,14 @@
+package demo7;
+
+/**
+ *测试demo8
+ *@date 2019/8/5
+ *@author danielHua
+ */
+public class Demo8
+{
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println("hello world!");
+    }
+}
