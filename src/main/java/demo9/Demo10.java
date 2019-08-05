cc84889b9aa16019bca5eec283901c6c466350b4
danielHua <zhengjh@ap-ec.cn>
2019-08-05 11:52:01 +0800

    12312312

---
+++ src/main/java/demo9/Demo10.java
@@ -0,0 +1,14 @@
+package demo9;
+
+/**
+ *
+ *@date 2019/8/5
+ *@author danielHua
+ */
+public class Demo10
+{
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println("hello world!");
+    }
+}
