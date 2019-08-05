96e48cafe8a1a8bdd0f4cc2fea85d25abc2651b7
danielHua <zhengjh@ap-ec.cn>
2019-08-05 11:07:54 +0800

    提交代码

---
+++ src/main/java/demo5/Demo6.java
@@ -0,0 +1,14 @@
+package demo5;
+
+
+/**
+ * 测试demo
+ *
+ * @author daniel
+ */
+public class Demo6
+{
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
