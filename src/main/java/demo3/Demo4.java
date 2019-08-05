6b63b3ad60b4c2b33f6aa07f78ad3322d7a654c3
danielHua <zhengjh@ap-ec.cn>
2019-08-05 11:04:10 +0800

    666666

---
+++ src/main/java/demo3/Demo4.java
@@ -0,0 +1,13 @@
+package demo3;
+
+/**
+ * 测试demo
+ *
+ * @author daniel
+ */
+public class Demo4
+{
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
---
+++ src/main/java/demo4/Demo5.java
@@ -0,0 +1,13 @@
+package demo4;
+
+/**
+ * 测试demo
+ *
+ * @author daniel
+ */
+public class Demo5
+{
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
